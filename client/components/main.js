ab84cbadcd730b61cec25c06cba0fb17fc669ced
Evlis Henry <ehenry692@hotmail.com>
2017-12-30 08:34:07 -0500

    Update main.js

--- client/components/main.js
+++ client/components/main.js
@@ -10,19 +10,19 @@ export default function Main (props) {
     <div className="nav-wrapper black">
       <div href="/" id="name" className="brand-logo"><h2>Evlis Henry</h2></div>
       <a data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-      <ul className="right hide-on-med-and-down">
-        <li><Link to="/about"><h5>About</h5></Link></li>
-        <li><h5>|</h5></li>
-        <li><Link to="/work"><h5>Projects</h5></Link></li>
-        <li><h5>|</h5></li>
-        <li><Link to="/contact"><h5>Contact</h5></Link></li>
-        <li><h5>|</h5></li>
-        {/* <li><Link to="/resume"><h5>Resume</h5></Link></li> */}
-      </ul>
-      <ul className="side-nav" id="mobile-demo side-nav">
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/Work">Work</Link></li>
-      </ul>
+//       <ul className="right hide-on-med-and-down">
+//         <li><Link to="/about"><h5>About</h5></Link></li>
+//         <li><h5>|</h5></li>
+//         <li><Link to="/work"><h5>Projects</h5></Link></li>
+//         <li><h5>|</h5></li>
+//         <li><Link to="/contact"><h5>Contact</h5></Link></li>
+//         <li><h5>|</h5></li>
+//         {/* <li><Link to="/resume"><h5>Resume</h5></Link></li> */}
+//       </ul>
+//       <ul className="side-nav" id="mobile-demo side-nav">
+//         <li><Link to="/about">About</Link></li>
+//         <li><Link to="/Work">Work</Link></li>
+//       </ul>
     </div>
   </nav>
       <hr />
