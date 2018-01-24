97407b1426a0e42e64e90823769d80e80da51f4f
EHenry92 <EHenry692@hotmail.com>
2018-01-24 16:10:36 -0500

    small change

--- client/components/Home.js
+++ client/components/Home.js
@@ -37,12 +37,6 @@ const Home = () => {
     }
     function highlight(evt) {
       evt.preventDefault();
-      // const oldSpots = document.getElementsByClassName('highlight');
-      // console.log("oldSpots", oldSpots)
-      // for (let j = 0; j < oldSpots.length ; j= j + 1) {
-      //   console.log("here", oldSpots[j])
-      //   oldSpots[j].classList.remove('highlight');
-      // }
       const spots = document.getElementsByClassName(evt.target.name);
       for (let i = 0; i < spots.length ; i++) {
         spots[i].classList.add('highlight-char-input');
@@ -52,13 +46,8 @@ const Home = () => {
       evt.preventDefault();
       let oldSpots = document.getElementsByClassName('highlight-char-input');
       for (let j = oldSpots.length; j > 0 ; j -= 1) {
-        console.log(oldSpots.length, j)
         oldSpots[0].classList.remove('highlight-char-input');
       }
-      // const spots = document.getElementsByClassName(evt.target.name);
-      // for (let i = 0; i < spots.length ; i++) {
-      //   spots[i].classList.add('highlight');
-      // }
     }
 
     let words = "HEY, I'M EVLIS HENRY, A DEVELOPER, ENTHUSIASTIC PUZZLE SOLVER AND NATURALLY CURIOUS INDIVIDUAL.".split(' ').map(word => {return word.split('')});
