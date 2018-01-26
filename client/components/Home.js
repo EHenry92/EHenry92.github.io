48dabe76ae2fe1d8042ceb122b3a6c001211223a
EHenry92 <EHenry692@hotmail.com>
2018-01-25 19:45:16 -0500

    limit input on mobile

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,37 +2,36 @@
 import React from 'react';
 
 const Home = () => {
-  // document.onreadystatechange = () => {
-  //   if (document.readyState === 'complete') {
-  //     setTimeout(() => {
-  //       document.getElementById('startUp').style.display = 'block';
-  //     }, 3000)
-  //   }
-  // };
-
   function startReveal (evt) {
       evt.preventDefault();
       document.getElementById('startUp').style.display = 'none';
-        let list, cryptList, textnode, waitTime = 750;
+        let waitTime = 750;
         for (let i = 0; i < randAlpha.length; i++) {
-          setTimeout(() => {
-            list = document.getElementsByClassName(`${randAlpha[i]}spot`);
-            cryptList = document.getElementsByClassName(`${randAlpha[i]}letter`);
+          setTimeout(revealLetter(randAlpha[i]), waitTime * i )
+      }
+    }
+    function revealLetter (letter) {
+      let list, cryptList, textnode;
+      list = document.getElementsByClassName(`${letter}spot`);
+            cryptList = document.getElementsByClassName(`${letter}letter`);
 
             for (let j = 0; j < list.length; j++) {
-              textnode = document.createTextNode(`${randAlpha[i]}`);
+              textnode = document.createTextNode(`${letter}`);
               list[j].removeChild(list[j].firstChild);
               list[j].appendChild(textnode);
               cryptList[j].classList.add('solved');
             }
-          }, waitTime * i )
-      }
     }
     function enterChar(evt) {
       evt.preventDefault();
+      let input = evt.target.value ? evt.target.value[0].toUpperCase() : ' ';
       let spots = document.getElementsByClassName(evt.target.name);
+
       for (let i = 0; i < spots.length ; i++) {
-        spots[i].firstChild.value = evt.target.value.toUpperCase();
+        if (input == evt.target.name[0]) {
+          revealLetter(input)
+        }
+        spots[i].firstChild.value = input;
       }
     }
     function highlight(evt) {
@@ -83,7 +82,7 @@ const Home = () => {
                     holder[char] ?
                         (<span className="crypto char" key={char + wordIdx + idx}>
                             <div className={`${char}spot cryptoReveal`}>
-                              <input name={`${char}spot`} onChange={enterChar} onMouseEnter={highlight}maxLength= {1} onMouseLeave={lowlight}/>
+                              <input name={`${char}spot`} onChange={enterChar} onMouseEnter={highlight} maxLength= {1} onMouseLeave={lowlight}/>
                             </div>
                             <div className={`${char}letter cryptoLetter`}>{holder[char]}</div>
                         </span>)
