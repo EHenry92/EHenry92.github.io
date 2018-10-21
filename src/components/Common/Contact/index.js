94c61a54637be8ba9a6bf4bc72bdddc9d9bbb502
EHenry92 <EHenry692@hotmail.com>
2018-10-21 10:04:08 -0400

    contact

--- src/components/Common/Contact/index.js
+++ src/components/Common/Contact/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 
 const Contact = () => {
   return (
@@ -6,18 +7,18 @@ const Contact = () => {
       <span className="category">
           <a href="mailto:Evlis.Henry@gmail.com"><img
           className= "tinyImg"
-          src="client/components/images/social-mail.png" /></a>
+          src="src/images/social-mail.png" /></a>
       </span>
       <span className="category">
         <a href="https://www.github.com/ehenry92"><img
         className= "tinyImg"
-        src="client/components/images/social-git.png" /></a>
+        src="src/images/social-git.png" /></a>
       </span>
       <span  className="category">
-        <a href="https://www.likedin.com/in/evils-henry" ><img  className= "tinyImg" src="client/components/images/social-linked.png" /></a>
+        <a href="https://www.likedin.com/in/evils-henry" ><img  className= "tinyImg" src="src/images/social-linked.png" /></a>
       </span>
       <span  className="category">
-        <a href="https://medium.com/@Silve"><img  className= "tinyImg" src="client/components/images/social-med.png" /></a>
+        <a href="https://medium.com/@Silve"><img  className= "tinyImg" src="src/images/social-med.png" /></a>
       </span>
     </div>
   )
