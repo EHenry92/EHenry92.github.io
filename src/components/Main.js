4344702b92b2de459f7b376550285db5842e427f
EHenry92 <EHenry692@hotmail.com>
2018-10-21 08:44:14 -0400

    initial setup

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import PropTypes from "prop-types";
+const Main = () => <div> this i smain </div>
+
+export default Main;
+
+// Main.propTypes = {
+//   label: PropTypes.string.isRequired,
+//   text: PropTypes.string.isRequired,
+//   type: PropTypes.string.isRequired,
+//   id: PropTypes.string.isRequired,
+//   value: PropTypes.string.isRequired,
+//   handleChange: PropTypes.func.isRequired
+// };
