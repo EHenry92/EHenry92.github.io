943f255f8b5480e03153f451a0f3b73a8eefbdde
EHenry92 <EHenry692@hotmail.com>
2018-11-03 19:31:58 -0400

    html in root

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   output: {
     path: __dirname + '/docs',
@@ -30,10 +29,4 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
 };
